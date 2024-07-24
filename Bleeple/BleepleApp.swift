822f1f6f64a341b3b1304abc626635d7f7fe3cb2
Corné <corne@cp3.io>
2024-07-24 19:14:46 +0200

    Initial Commit

---
+++ Bleeple/BleepleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BleepleApp.swift
+//  Bleeple
+//
+//  Created by Corné on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct BleepleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
