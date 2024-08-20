4a826cce0ea5206dc0d5257bc3373583a84fc4f1
Corné <corne@cp3.io>
2024-08-20 17:34:44 +0200

    fixed tests

--- BleepleTests/MainViewModelTests.swift
+++ BleepleTests/MainViewModelTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 final class MainViewModelTests: XCTestCase {
     func testUndoRedo() throws {
-        let viewModel = MainView.ViewModel()
+        let viewModel = MainView.ViewModel.shared
             
         // Check initial state
         XCTAssert(viewModel.events.isEmpty, "Initial events should be empty")
